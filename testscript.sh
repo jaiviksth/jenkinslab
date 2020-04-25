4c402c76e4c1e353eea70ece87fcba7994cb70a8
Jay Shrestha <isolveproblemsforall@gmail.com>
2020-04-24 21:03:28 -0400

    2nd change

--- testscript.sh
+++ testscript.sh
@@ -2,9 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added"
-echo "This was added again"
-echo "The final number is $NUMBER"
-echo "This is 4th Line"
-echo "This is 5th Line"
-echo "This is 6th Line"
+echo "This is change number $NUMBER"
